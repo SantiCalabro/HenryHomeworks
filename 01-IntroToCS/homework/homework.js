92b5f0f895162b739305023482f7a7cba1e3e2fa
Santiago <santi.calabro93@gmail.com>
2022-06-18 21:23:47 -0300

    M1 homework concluded

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,11 +2,29 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  const reverse = num.split('').reverse().join('');
+  var arr = []
 
+  for ( var i= 0; i< reverse.length;i++){
+  arr.push(Math.pow(2, [i])*reverse[i])
+  }
+  
+  var final = arr.reduce((acc,item)=>{
+     return acc+item
+  })
+  
+  return final
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  var arr = []
+  while(num > 0){
+    arr.push(num%2)
+    num = parseInt(num/2)
+  }
+  var final = arr.reverse().join("")
+  return final
 
 }
 
